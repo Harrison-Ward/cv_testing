08acb92f7b07459f6cbf032139ac9336f47ea2a5
Harrison Ward <hward4116@gmail.com>
2023-11-24 22:22:11 -0800

    updated the function to wrap writing and luminosity transform functionality into a function call

--- main/ascii.cpp
+++ main/ascii.cpp
@@ -5,11 +5,10 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char*argv[])
 {
-    // ensure that an file path is passed in at execution
-    if (argc < 2)
-    {
+    // check that a file path has been provided
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <image_path>" << std::endl;
         return 1;
     }
@@ -17,66 +16,43 @@ int main(int argc, char *argv[])
     // ascii conversion grey scale
     std::string g_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
-    // loop over the argument vector, starting from first file path index and store images
+    // read in images from the file paths provided in the argument vector
     std::vector<cv::Mat> images;
-    for (int i = 1; i < argc; i++)
-    {
-        cv::Mat img = cv::imread(argv[i], cv::IMREAD_GRAYSCALE);
+    for (int i = 1; i < argc; i++) 
+    {   
+        // define grey scale map
+        std::string g_scale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+        
+        auto start_load = std::chrono::high_resolution_clock::now();
 
-        if (img.empty())
-        {
+        cv::Mat img = cv::imread(argv[i], cv::IMREAD_GRAYSCALE);
+        if (img.empty()) {
             std::cerr << "Error: Image at " << argv[i] << " could not be loaded." << std::endl;
             continue; // Skip this image and continue with the next one
         }
+
+        // measure load time
+        auto end_load = std::chrono::high_resolution_clock::now();
+        auto load_duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_load - start_load).count();
+        std::cout << "\n image loaded in " << load_duration << " miliseconds" << endl;
+
+        // resize the image to aid display
         cv::resize(img, img, cv::Size(), 0.5, 0.5);
-        // store images in the image vector
-        images.push_back(img);
+        // create the output file path based on the name of the input image
+        auto file_name = static_cast<string>(argv[i]);
+        size_t extension_position = file_name.find_last_of('.');
+
+        auto output_file_path = file_name.substr(0, extension_position);
+        output_file_path += ".txt";
+        
+        // convert the image to ACII and write it to the output folder, time it
+        auto start = std::chrono::high_resolution_clock::now();
+        image_to_ascii(img, output_file_path, 5, g_scale);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        // display time to generate
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << output_file_path << "generated in: " << duration << " miliseconds" << endl;
     }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    // define kernel size for the image, smaller values for higher resolution
-    cv::Size kernel_size(10, 10);
-    std::vector<cv::Mat> luminosity_matricies;
-
-    for (const auto &img : images)
-    {
-        // translate the images to their luminosity values
-        cv::Mat luminosity_matrix;
-        cv::boxFilter(img, luminosity_matrix, -1, kernel_size);
-        luminosity_matricies.push_back(luminosity_matrix);
-    }
-
-    std::vector<std::vector<std::string>> ascii_images;
-    // hash the luminosity values to their respective ascii chars
-    for (auto &luminosity_matrix : luminosity_matricies)
-    {
-        std::vector<std::string> output_ascii;
-        luminosity_to_ascii(luminosity_matrix, output_ascii, g_scale);
-
-        ascii_images.push_back(output_ascii);
-    }
-
-    // loop over the images and display
-    int i = 1;
-    for (const auto &ascii : ascii_images)
-    {
-        string file_name;
-        file_name += static_cast<string>(argv[i]);
-        file_name = file_name.erase(file_name.length() - 5);
-        file_name += ".txt";
-
-        cout << file_name << endl;
-
-        ofstream outFile(file_name);
-        for (const auto &line : ascii)
-        {
-            outFile << line << "\n";
-        }
-        outFile.close();
-        i++;
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "Time taken to generate ASCII art: " << duration << " milliseconds." << std::endl;
     return 0;
 }
